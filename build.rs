3e19c546a8a6fe9786a3dfab2585be6cc6d704f2
Jan Ekström <jeebjp@gmail.com>
2019-11-13 01:03:33 +0200

    build: ignore certain math related macros
    bindgen otherwise fails.
    
    Picked up from https://github.com/bluejekyll/pg-extend-rs/pull/8/files
    as it seemed to be more intelligent than the "classic" way of
    handling these.

--- build.rs
+++ build.rs
@@ -1,12 +1,39 @@
 extern crate bindgen;
 extern crate pkg_config;
 
+use std::collections::HashSet;
 use std::env;
 use std::path::PathBuf;
+
+#[derive(Debug)]
+struct IgnoreMacros(HashSet<String>);
+
+impl bindgen::callbacks::ParseCallbacks for IgnoreMacros {
+    fn will_parse_macro(&self, name: &str) -> bindgen::callbacks::MacroParsingBehavior {
+        if self.0.contains(name) {
+            bindgen::callbacks::MacroParsingBehavior::Ignore
+        } else {
+            bindgen::callbacks::MacroParsingBehavior::Default
+        }
+    }
+}
 
 fn main() {
     let used_libraries = ["libavcodec", "libavformat", "libavutil"];
     let mut include_paths = Vec::new();
+    let ignored_macros = IgnoreMacros(
+        vec![
+            "FP_INFINITE".into(),
+            "FP_NAN".into(),
+            "FP_NORMAL".into(),
+            "FP_SUBNORMAL".into(),
+            "FP_ZERO".into(),
+            "IPPORT_RESERVED".into(),
+        ]
+        .into_iter()
+        .collect(),
+    );
+
 
     for library in used_libraries.iter() {
         if let Ok(lib) = pkg_config::find_library(library) {
@@ -26,8 +53,8 @@ fn main() {
     }
 
     let bindings = bindgen::Builder::default()
-        .no_unstable_rust()
         .clang_args(include_paths)
+        .parse_callbacks(Box::new(ignored_macros))
         .header("./helpers/bindings_required.h")
         .generate()
         .expect("Unable to generate bindings");
